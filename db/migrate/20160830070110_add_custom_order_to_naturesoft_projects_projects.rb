b70b4ecca98498e3615012fedbc41543511224b8
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-03 07:45:39 -0700

    update custom order for projects

--- db/migrate/20160830070110_add_custom_order_to_naturesoft_projects_projects.rb
+++ db/migrate/20160830070110_add_custom_order_to_naturesoft_projects_projects.rb
@@ -1,5 +1,5 @@
 class AddCustomOrderToNaturesoftProjectsProjects < ActiveRecord::Migration[5.0]
   def change
-    add_column :naturesoft_projects_projects, :custom_order, :string, default: 0
+    add_column :naturesoft_projects_projects, :custom_order, :integer, default: 0
   end
 end
